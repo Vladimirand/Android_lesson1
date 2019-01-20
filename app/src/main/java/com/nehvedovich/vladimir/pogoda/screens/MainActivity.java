4fb62951e92f8beb963e16992d6d208a047b050d
Vladimirand <vladimir19051992@mail.ru>
2019-01-20 15:13:02 +0300

    lesson2. Изменил структуру файлов по анологии с занятием №2. Записал последовательность вызова методов при различных действиях пользователей: при запуске приложения, при переворачивании устройства, при нажатии кнопки "Домой".

---
+++ app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/MainActivity.java
@@ -0,0 +1,53 @@
+package com.nehvedovich.vladimir.pogoda.screens;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nehvedovich.vladimir.pogoda.R;
+import com.nehvedovich.vladimir.pogoda.utils.ViewUtilities;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewUtilities.makeToast(getApplicationContext(), "onCreate");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ViewUtilities.makeToast(getApplicationContext(), "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ViewUtilities.makeToast(getApplicationContext(), "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ViewUtilities.makeToast(getApplicationContext(), "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        ViewUtilities.makeToast(getApplicationContext(), "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ViewUtilities.makeToast(getApplicationContext(), "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        ViewUtilities.makeToast(getApplicationContext(), "onRestart");
+    }
+}
