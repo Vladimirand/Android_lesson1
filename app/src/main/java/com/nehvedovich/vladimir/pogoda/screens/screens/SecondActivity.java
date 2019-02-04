248dc701775e89d4ea1c482a2284291ad228bbfb
Vladimirand <vladimir19051992@mail.ru>
2019-02-04 16:14:09 +0300

    Доработка кода.

--- app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/screens/SecondActivity.java
+++ app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/screens/SecondActivity.java
@@ -13,8 +13,6 @@ public class SecondActivity extends AppCompatActivity {
 
     public static final String CITY_NAME_EXSTRA = "cityLookingFor";
 
-//    public static  final Boolean WIND = true;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +20,19 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         Bundle bundle = getIntent().getExtras();
+        boolean wind = false;
+        boolean humidity = false;
+        boolean pressure = false;
         if (bundle != null) {
             TextView cityName = findViewById(R.id.cityNameInfo);
             cityName.setText(bundle.getString(CITY_NAME_EXSTRA));
+
+            wind = bundle.getBoolean(MainActivity.CHECK_BOX_WIND);
+            humidity = bundle.getBoolean(MainActivity.CHECK_BOX_HUMIDITY);
+            pressure = bundle.getBoolean(MainActivity.CHECK_BOX_PRESSURE);
         }
 
-
-//        ViewUtilities.makeToast(getApplicationContext(), "onCreate");
+//      ViewUtilities.makeToast(getApplicationContext(), "onCreate");
 
         ImageButton toHome = findViewById(R.id.toHome);
         toHome.setOnClickListener(new View.OnClickListener() {
@@ -39,16 +43,34 @@ public class SecondActivity extends AppCompatActivity {
         });
 
 
-//Обработка CheckBox Wind
-//        final TextView textWind = (TextView) findViewById(R.id.textWind);
-//        final TextView textDirection = (TextView) findViewById(R.id.directionWind);
-//
-//        if (WIND == true) {
-//            textWind.setVisibility(View.VISIBLE);
-//            textDirection.setVisibility(View.VISIBLE);
-//        } else {
-//            textWind.setVisibility(View.GONE);
-//            textDirection.setVisibility(View.GONE);
-//        }
+        //Обработка CheckBox Wind
+        TextView textWind = (TextView) findViewById(R.id.textWind);
+        TextView textDirection = (TextView) findViewById(R.id.directionWind);
+
+        if (wind == true) {
+            textWind.setVisibility(View.VISIBLE);
+            textDirection.setVisibility(View.VISIBLE);
+        } else {
+            textWind.setVisibility(View.GONE);
+            textDirection.setVisibility(View.GONE);
+        }
+
+        //Обработка CheckBox humidity
+        TextView textHumidity = (TextView) findViewById(R.id.textHumidity);
+
+        if (humidity == true) {
+            textHumidity.setVisibility(View.VISIBLE);
+        } else {
+            textHumidity.setVisibility(View.GONE);
+        }
+
+        //Обработка CheckBox Pressure
+        TextView textPressure = (TextView) findViewById(R.id.textPressure);
+
+        if (pressure == true) {
+            textPressure.setVisibility(View.VISIBLE);
+        } else {
+            textPressure.setVisibility(View.GONE);
+        }
     }
 }
