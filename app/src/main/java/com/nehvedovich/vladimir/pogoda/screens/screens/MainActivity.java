2026a4411cf44411a50fd9b3867ed58a43a5ea02
Vladimirand <vladimir19051992@mail.ru>
2019-02-07 20:25:53 +0300

    Перевод во фрагменты элементов основного и второго экранов. На данном этапе не решен вопрос передачи данных от первого фрагмента ко второму.

--- app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/screens/MainActivity.java
+++ app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/screens/MainActivity.java
@@ -4,53 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.ImageButton;
 
 import com.nehvedovich.vladimir.pogoda.R;
 
-
 public class MainActivity extends AppCompatActivity {
-
-    public static final String CHECK_BOX_WIND = "checkBoxWind";
-    public static final String CHECK_BOX_HUMIDITY = "checkBoxHumidity";
-    public static final String CHECK_BOX_PRESSURE = "checkBoxPressure";
-
-
-    private final View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            Button btn = findViewById(v.getId());
-            String cityName = (String) btn.getText();
-
-            if (cityName != null) {
-                intent.putExtra(SecondActivity.CITY_NAME_EXSTRA, cityName);
-            }
-            intent.putExtra(CHECK_BOX_WIND, getCheckBoxWind());
-            intent.putExtra(CHECK_BOX_HUMIDITY, getCheckBoxHumidity());
-            intent.putExtra(CHECK_BOX_PRESSURE, getCheckBoxPressure());
-            startActivity(intent);
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        Button button = findViewById(R.id.to_city_1);
-        Button button2 = findViewById(R.id.to_city_2);
-        Button button3 = findViewById(R.id.to_city_3);
-        Button button4 = findViewById(R.id.to_city_4);
-        button.setOnClickListener(onClickListener);
-        button2.setOnClickListener(onClickListener);
-        button3.setOnClickListener(onClickListener);
-        button4.setOnClickListener(onClickListener);
-
-        setTheme(R.style.SettingsTheme2);
 
         ImageButton settings = findViewById(R.id.settings);
 
@@ -61,20 +24,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    public Boolean getCheckBoxWind() {
-        CheckBox wind = findViewById(R.id.checkBoxWind);
-        return wind.isChecked();
-    }
-
-    public Boolean getCheckBoxHumidity() {
-        CheckBox humidity = findViewById(R.id.checkBoxHumidity);
-        return humidity.isChecked();
-    }
-
-    public Boolean getCheckBoxPressure() {
-        CheckBox pressure = findViewById(R.id.checkBoxPressure);
-        return pressure.isChecked();
-    }
-
 }
--- app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/screens/SecondActivity.java
+++ app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/screens/SecondActivity.java
@@ -1,11 +1,9 @@
 package com.nehvedovich.vladimir.pogoda.screens.screens;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,41 +20,14 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Bundle bundle = getIntent().getExtras();
-        boolean wind = false;
-        boolean humidity = false;
-        boolean pressure = false;
-        if (bundle != null) {
-            TextView cityName = findViewById(R.id.cityNameInfo);
-            cityName.setText(bundle.getString(CITY_NAME_EXSTRA));
-
-            wind = bundle.getBoolean(MainActivity.CHECK_BOX_WIND);
-            humidity = bundle.getBoolean(MainActivity.CHECK_BOX_HUMIDITY);
-            pressure = bundle.getBoolean(MainActivity.CHECK_BOX_PRESSURE);
+        if (savedInstanceState == null) {
+            CityInfoFragment details = new CityInfoFragment();
+            details.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
         }
-
-//      ViewUtilities.makeToast(getApplicationContext(), "onCreate");
 
         ImageButton toHome = findViewById(R.id.toHome);
         ImageButton share = findViewById(R.id.imageButtonShare);
-        Button details = findViewById(R.id.moreInformation);
-
-        details.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                TextView city = findViewById(R.id.cityNameInfo);
-                String cityName = (String) city.getText();
-                Uri uri = Uri.parse("http://yandex.ru/pogoda/" + cityName);
-                intent.setData(uri);
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(SecondActivity.this, "\n" +
-                            "Application does not exist", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
         toHome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,35 +56,5 @@ public class SecondActivity extends AppCompatActivity {
                 }
             }
         });
-
-        //Обработка CheckBox Wind
-        TextView textWind = (TextView) findViewById(R.id.textWind);
-        TextView textDirection = (TextView) findViewById(R.id.directionWind);
-
-        if (wind == true) {
-            textWind.setVisibility(View.VISIBLE);
-            textDirection.setVisibility(View.VISIBLE);
-        } else {
-            textWind.setVisibility(View.GONE);
-            textDirection.setVisibility(View.GONE);
-        }
-
-        //Обработка CheckBox humidity
-        TextView textHumidity = (TextView) findViewById(R.id.textHumidity);
-
-        if (humidity == true) {
-            textHumidity.setVisibility(View.VISIBLE);
-        } else {
-            textHumidity.setVisibility(View.GONE);
-        }
-
-        //Обработка CheckBox Pressure
-        TextView textPressure = (TextView) findViewById(R.id.textPressure);
-
-        if (pressure == true) {
-            textPressure.setVisibility(View.VISIBLE);
-        } else {
-            textPressure.setVisibility(View.GONE);
-        }
     }
 }
