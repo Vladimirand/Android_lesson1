48ccb5551a181be127fba3298cddd123e627c032
Vladimirand <vladimir19051992@mail.ru>
2019-04-07 00:38:01 +0300

    Д/з к уроку A2_L3.  Создал шаблон сервиса для считывания информации о погоде.

---
+++ app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/utils/BackgroundService.java
@@ -0,0 +1,87 @@
+package com.nehvedovich.vladimir.pogoda.screens.utils;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import com.nehvedovich.vladimir.pogoda.R;
+
+
+public class BackgroundService extends IntentService implements SensorEventListener {
+
+    private static String infoTemperature;
+
+    int messageId = 0;
+    private SensorManager mSensorManager;
+    private Sensor mTemperature;
+
+    public BackgroundService() {
+        super("BackgroundService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        //Здесь уже идет фоновая работа, не требующая
+        // создания своего потока
+        startSensor();
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float ambient_temperature = event.values[0];
+        infoTemperature = String.format("%.0f", ambient_temperature) + " ℃";
+        makeNote(getString(R.string.current_temperature));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+
+    private void startSensor() {
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        mSensorManager.registerListener(this, mTemperature, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    // Вывод уведомления в строке состояния
+    private void makeNote(String message){
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle(infoTemperature);
+        builder.setContentText(message);
+        Intent resultIntent = new Intent(this, BackgroundService.class);
+        //Для облегчения восприятия лучше воспользоваться методом
+//        PendingIntent.getActivity(this,0, resultIntent,
+//                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(messageId++, builder.build());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSensorManager.unregisterListener(this);
+    }
+}
