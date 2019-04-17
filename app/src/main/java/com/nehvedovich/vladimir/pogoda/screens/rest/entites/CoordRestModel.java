150cffe2f549da8f2a763c583dee12f7e7e7405e
Vladimirand <vladimir19051992@mail.ru>
2019-04-17 13:24:54 +0300

    В приложении сделал загрузку текущей погоды через Retrofit.

---
+++ app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/rest/entites/CoordRestModel.java
@@ -0,0 +1,8 @@
+package com.nehvedovich.vladimir.pogoda.screens.rest.entites;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CoordRestModel {
+    @SerializedName("lon") public float lon;
+    @SerializedName("lat") public float lat;
+}
