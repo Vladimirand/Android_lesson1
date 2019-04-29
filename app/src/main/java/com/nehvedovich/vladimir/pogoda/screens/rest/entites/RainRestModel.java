5dc81be40adefae3c7cacbc55558f3219f05966f
Vladimirand <vladimir19051992@mail.ru>
2019-04-29 19:36:07 +0300

    Загрузка в Гит

---
+++ app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/rest/entites/RainRestModel.java
@@ -0,0 +1,8 @@
+package com.nehvedovich.vladimir.pogoda.screens.rest.entites;
+
+import com.google.gson.annotations.SerializedName;
+
+
+public class RainRestModel {
+    @SerializedName("3h") public float h3;
+}
