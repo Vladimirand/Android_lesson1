ffdf6424f6fb1062a5bbe6790509fce750130c84
Vladimirand <vladimir19051992@mail.ru>
2019-06-05 20:24:17 +0300

    добавлен код для прогноза погоды на сутки

---
+++ app/src/main/java/com/nehvedovich/vladimir/pogoda/screens/rest/entites/SnowRestModel.java
@@ -0,0 +1,8 @@
+package com.nehvedovich.vladimir.pogoda.screens.rest.entites;
+
+import com.google.gson.annotations.SerializedName;
+
+public class SnowRestModel {
+    @SerializedName("1h") public float h1;
+    @SerializedName("3h") public float h3;
+}
